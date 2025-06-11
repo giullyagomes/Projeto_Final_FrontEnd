a8fbf036b3f3b088957122a980422bdbbc0e4bd8
Giullya Gomes <gomes.giullyav@gmail.com>
2025-06-11 14:44:56 -0300

    Correções na página de login

--- src/app/servicos/backforapp-api/tornar-usuario-vendedor/index.js
+++ src/app/servicos/backforapp-api/tornar-usuario-vendedor/index.js
@@ -1,8 +1,23 @@
 import axios from "axios";
 
+const buscarUsuario = async (id, sessionToken) => {
+    const response = await axios.get(
+        `https://parseapi.back4app.com/classes/_User/${id}`,
+        {
+            headers: {
+                "Content-Type": "application/json",
+                "X-Parse-Application-Id": "K9uPsRuFvBzLDEaaPDwuxrxtxY94dZPDLmuwRaEm",
+                "X-Parse-REST-API-Key": "PoqZ54BGlFJnpJ1wTuvxSJ4gDvh2TbwjrxeeNrmN",
+                "X-Parse-Session-Token": sessionToken
+            }
+        }
+    );
+    return response.data;
+};
+
 const tornarUsuarioVendedor = async (id, sessionToken) => {
     try {
-        const resultado = await axios.put(
+        await axios.put(
             `https://parseapi.back4app.com/classes/_User/${id}`,
             {
                 tipo: "vendedor"
@@ -16,7 +31,26 @@ const tornarUsuarioVendedor = async (id, sessionToken) => {
                 }
             }
         );
-        return resultado;
+
+        const usuario = await buscarUsuario(id, sessionToken);
+
+        const { sessionToken, tipo, objectId, createdAt, ACL, updatedAt, authData, username,  ...dadosVendedor } = usuario;
+
+        await axios.post(
+            `https://parseapi.back4app.com/classes/Vendedor`,
+            dadosVendedor,
+        
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-Parse-Application-Id": "K9uPsRuFvBzLDEaaPDwuxrxtxY94dZPDLmuwRaEm",
+                    "X-Parse-REST-API-Key": "PoqZ54BGlFJnpJ1wTuvxSJ4gDvh2TbwjrxeeNrmN",
+                    "X-Parse-Session-Token": sessionToken 
+                }
+            }
+        );
+
+        return true;
     } catch (erro) {
         throw new Error("Falha ao realizar a ação de tornar usuário vendedor." + erro);
     }
